353e0d779728b15dc34dbcfe589fa2fa50d0b85b
David Zarate <davidzaratetrujillo@gmail.com>
2018-03-07 18:33:07 -0600

    Add Dashboard route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
     render() {
@@ -9,7 +10,9 @@ class App extends Component {
             <BrowserRouter>
                 <div className="App">
                     <Switch>
-
+                        <Route exact path="/" component={Login}/>
+                        <Route exact path="/dashboard" component={Dashboard}/>
+                        <Redirect from='*' to='/' />
                     </Switch>
                 </div>
             </BrowserRouter>
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <p>Dashboard component!</p>
+        );
+    }
+}
+
+export default Dashboard;
